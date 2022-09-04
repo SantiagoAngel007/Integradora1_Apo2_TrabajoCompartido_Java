774239e8072109dc7a2d7efc732b6eb87e84a2ac
SamuelGutierrez24 <1007370844@u.icesi.edu.co>
2022-09-03 21:31:34 -0500

    Arbol binario de score y leaderBoard

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,15 +1,21 @@
 package model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Controller {
 
-	private Score root;
-	private List<Score> listScores;
+	private User root;
+	private ArrayList<User> listScores;
 	private Board head;
 	private Board tail;
 
-	public void addScore() {
-		// TODO - implement Controller.addScore
-		throw new UnsupportedOperationException();
+
+	public Controller(){
+		
+	}
+	public String  addUserScore(int score, String nickname) {
+		return addUserScore(score, nickname,root);
 	}
 
 	/**
@@ -17,44 +23,72 @@ public class Controller {
 	 * @param score
 	 * @param current
 	 */
-	private void addScore(Score score, Score current) {
-		// TODO - implement Controller.addScore
-		throw new UnsupportedOperationException();
+	private String addUserScore(int score,String nickname, User current) {
+		
+		if(root== null){
+			User newUser = new User(score, nickname);
+			root = newUser;
+			return "Se agrego el puntaje";
+		}else{
+
+			if(current.getScore()<score){
+				
+				if(current.getRight()==null){
+					User user = new User(score, nickname);
+					current.setRight(user);
+				}else{
+					addUserScore(score, nickname, current.getRight());
+				}
+			}else{
+				
+				if(current.getLeft() == null){
+					User user = new User(score, nickname);
+					current.setLeft(user);
+					
+				}else{
+					addUserScore(score, nickname, current.getLeft());
+				}
+			}
+			return "Se agrego el puntaje a la tabla";
+		}
+		
+		
+		
 	}
 
-	/**
-	 * 
-	 * @param goal
-	 */
-	public Score searchScore(int goal) {
-		// TODO - implement Controller.searchScore
-		throw new UnsupportedOperationException();
-	}
+	
 
-	/**
-	 * 
-	 * @param current
-	 * @param target
-	 */
-	private Score searchScore(Score current, int target) {
-		// TODO - implement Controller.searchScore
-		throw new UnsupportedOperationException();
-	}
-
-	public List<Score> showScore() {
-		// TODO - implement Controller.showScore
-		throw new UnsupportedOperationException();
+	public ArrayList<User> scoreInOrder() {
+		return scoreInOrder(root);
 	}
 
 	/**
 	 * 
 	 * @param score
 	 */
-	private List<Score> showScore(Score score) {
-		// TODO - implement Controller.showScore
-		throw new UnsupportedOperationException();
-	}
+	private ArrayList<User> scoreInOrder(User user) {
+		if(user == null){
+            return listScores;
+        }
 
+        scoreInOrder(user.getLeft());
+        listScores.add(user);
+        scoreInOrder(user.getRight());
+        return listScores;
+	}
+	
+
+
+	public String showLeaderBoard(){
+		scoreInOrder();
+		String out = "";
+		int size = listScores.size();
+
+		for(int i = 0; i<size;i++){
+			out += "| " + listScores.get(i).getNickname() + " : " + listScores.get(i).getScore() + " |"; 
+		}
+		return out;
+	}
 	/**
 	 * 
 	 * @param pipe
